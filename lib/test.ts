0847aa6196eb3ec1edc7715c64e72ca105109b59
Taylor Stapleton <stapletontaylor@hotmail.com>
2017-11-18 11:26:29 -0700

    More work yet again.

--- lib/test.ts
+++
@@ -1,7 +0,0 @@
-import { twitch_api_library } from './twitch-api-library';
-
-let tal = new twitch_api_library('Client ID Here');
-let Games = tal.GetGames('Minecraft');
-Games.on('data', (data) => {
-  console.log(data.toString());
-});
