727d43e42040ec871c9d29b7c4e58f8ac55a1293
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-11-25 09:41:09 -0700

    Remove submodules

--- Alloybot.ts
+++ Alloybot.ts
@@ -3,7 +3,6 @@ import { Signale as SignaleType } from 'signale';
 import { format } from 'util';
 import path = require('path');
 import fs = require('fs');
-import { exec } from 'child_process';
 
 const Settings = require('./Settings.json');
 const { Signale } = require('signale');
@@ -17,136 +16,125 @@ fs.readdir(__dirname, (error, files) => {
   });
 });
 
-export interface IConnection {
-  name: String;
-  dependencies: String[];
-  connection: any;
-}
-
-export interface IModule {
-  name: String;
-  dependencies: String[];
-}
-
 export class Logger {
   private logger: SignaleType = new Signale(Settings.Signale);
-  private scope: String;
+  private scope: string;
 
-  constructor(scope: String) {
+  constructor(scope: string) {
     this.scope = scope;
   }
 
-  private formatMessage(message: Object | String): String {
+  private formatMessage(message: object | string): string {
     if (typeof message == 'object') return format('%o', message);
     if (typeof message == 'string') return format('%s', message);
   }
 
-  public await(message: Object | String): void {
+  public await(message: object | string): void {
     this.logger.await({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public complete(message: Object | String): void {
+  public complete(message: object | string): void {
     this.logger.complete({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public error(message: Object | String): void {
+  public error(message: object | string): void {
     this.logger.error({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public debug(message: Object | String): void {
+  public debug(message: object | string): void {
     this.logger.debug({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public fatal(message: Object | String): void {
+  public fatal(message: object | string): void {
     this.logger.fatal({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public fav(message: Object | String): void {
+  public fav(message: object | string): void {
     this.logger.fav({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public info(message: Object | String): void {
+  public info(message: object | string): void {
     this.logger.info({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public log(message: Object | String): void {
+  public log(message: object | string): void {
     this.logger.log({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public note(message: Object | String): void {
+  public note(message: object | string): void {
     this.logger.note({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public pause(message: Object | String): void {
+  public pause(message: object | string): void {
     this.logger.pause({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public pending(message: Object | String): void {
+  public pending(message: object | string): void {
     this.logger.pending({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public star(message: Object | String): void {
+  public star(message: object | string): void {
     this.logger.star({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public start(message: Object | String): void {
+  public start(message: object | string): void {
     this.logger.start({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public success(message: Object | String): void {
+  public success(message: object | string): void {
     this.logger.success({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public warn(message: Object | String): void {
+  public warn(message: object | string): void {
     this.logger.warn({
       prefix: this.scope,
       message: this.formatMessage(message)
     });
   }
 
-  public watch(message: Object | String): void {
+  public watch(message: object | string): void {
     this.logger.watch({
       prefix: this.scope,
       message: this.formatMessage(message)
@@ -177,104 +165,155 @@ export class Loader {
   }
 }
 
-class RootModuleLoader {
-  private root = './src/modules';
+class PluginLoader {
+  private root = './src/plugins';
   constructor() {
-    fs.readdir(this.root, (error, modules) => {
+    fs.readdir(this.root, (error, plugins) => {
       if (error) console.error(error);
-      modules.forEach(_module => {
-        let modulepath = path.join(this.root, _module);
-        let _package = require('./' + path.join(modulepath, 'package.json'));
+      plugins.forEach(_module => {
+        let pluginpath = path.join(this.root, _module);
+        let _package = require('./' + path.join(pluginpath, 'package.json'));
 
         if (_package.main.endsWith('.js'))
-          require('./' + path.join(modulepath, _package.main));
+          require('./' + path.join(pluginpath, _package.main));
         if (_package.main.endsWith('.ts')) {
           require('./' +
-            path.join(modulepath, _package.main.replace('.ts', '.js')));
+            path.join(pluginpath, _package.main.replace('.ts', '.js')));
         }
       });
     });
   }
 }
 
+export type DependantList = (IPlugin | IConnection)[];
+
+export interface IConnection {
+  readonly name: string;
+  readonly dependencies: string[];
+  readonly dependants: DependantList;
+  connection: any;
+}
+
+export interface IPlugin {
+  readonly name: string;
+  readonly dependencies: string[];
+  readonly dependants: DependantList;
+}
+
 class Alloybot extends EventEmitter {
-  public name: String = process.env['ALLOYBOT_NAME'];
-  public connections = new Map<String, IConnection>();
-  public modules = new Map<String, IModule>();
+  public name: string = process.env['ALLOYBOT.NAME'];
+  public connections = new Map<string, IConnection>();
+  public plugins = new Map<string, IPlugin>();
   public settings = Settings;
 
   private logger = new Logger(this.name);
 
   constructor() {
     super();
-    new RootModuleLoader();
+    new PluginLoader();
     this.emit('started', this.name);
   }
 
-  private dependenciesAreLoaded(name: String): Boolean {
-    let dependencyList = this.modules.get(name).dependencies,
-      dependencyCount = 0,
+  private areDependenciesLoaded(name: string): boolean {
+    let dependencyList = this.plugins.get(name).dependencies,
+      loadedDeps = [],
       missingDeps = [];
 
     for (let dep in dependencyList) {
-      if (this.modules.has(dep)) {
-        dependencyCount++;
+      if (this.plugins.has(dep)) {
+        loadedDeps.push(dep);
       } else {
         missingDeps.push(dep);
       }
     }
 
-    if (dependencyCount == dependencyList.length) {
+    if (loadedDeps == dependencyList) {
       return true;
     } else {
       this.logger.error(
-        `${name} is missing ${dependencyCount}/${
+        `${name} is missing ${missingDeps.length}/${
           dependencyList.length
         } dependencies. - ${missingDeps.join(', ')}`
       );
+      return false;
     }
   }
 
-  public registerModule(moduleClass: IModule): void {
-    this.modules.set(moduleClass.name, moduleClass);
-    this.emit('module.registered', moduleClass);
+  public getDependants(name: string): DependantList {
+    let dependants = [];
+    for (let plugin in this.plugins) {
+      for (let dep in this.plugins.get(plugin).dependencies) {
+        if (dep == name) dependants.push(this.plugins.get(plugin));
+      }
+    }
+    for (let connection in this.connections) {
+      for (let dep in this.connections.get(connection).dependencies) {
+        if (dep == name) dependants.push(this.connections.get(connection));
+      }
+    }
+    return dependants;
   }
 
-  public isModuleLoaded(name: String): Boolean {
-    return this.modules.has(name);
+  public isPluginLoaded(plugin: IPlugin): boolean;
+  public isPluginLoaded(plugin: string): boolean;
+  public isPluginLoaded(plugin): boolean {
+    if (typeof plugin == 'string') {
+      return this.plugins.has(plugin);
+    } else {
+      return this.plugins.has(plugin.name);
+    }
   }
 
-  public getModuleCount(): Number {
-    return this.modules.size;
+  public registerPlugin(plugin: IPlugin): void {
+    if (this.plugins.has(plugin.name)) {
+      this.emit('plugin.duplicate', plugin.name);
+    } else {
+      this.plugins.set(plugin.name, plugin);
+      this.emit('plugin.registered', plugin);
+    }
   }
 
-  public getModules(): Map<String, IModule> {
-    return this.modules;
+  public getPluginCount(): number {
+    return this.plugins.size;
   }
 
-  public getModule(name: String): any {
-    return this.modules.get(name);
+  public getPlugins(): Map<string, IPlugin> {
+    return this.plugins;
+  }
+
+  public getPlugin(name: string): any {
+    if (this.areDependenciesLoaded(name)) return this.plugins.get(name);
   }
 
   public registerConnection(connection: IConnection): void {
-    this.connections.set(connection.name, connection.connection);
-    this.emit('connection.registered', connection);
+    if (this.connections.has(connection.name)) {
+      this.emit('connection.duplicate', connection.name);
+    } else {
+      this.connections.set(connection.name, connection.connection);
+      this.emit('connection.registered', connection);
+    }
   }
 
-  public isConnectionLoaded(name: String): Boolean {
-    return this.connections.has(name);
+  public isConnectionLoaded(connection: IConnection): boolean;
+  public isConnectionLoaded(connection: string): boolean;
+  public isConnectionLoaded(connection): boolean {
+    if (typeof connection == 'string') {
+      return this.connections.has(connection);
+    } else {
+      return this.connections.has(connection.name);
+    }
   }
 
-  public getConnectionCount(): Number {
+  public getConnectionCount(): number {
     return this.connections.size;
   }
 
-  public getConnections(): Map<String, IConnection> {
+  public getConnections(): Map<string, IConnection> {
     return this.connections;
   }
 
-  public getConnection(name: String): any {
-    return this.connections.get(name);
+  public getConnection(name: string): any {
+    if (this.areDependenciesLoaded(name)) return this.connections.get(name);
   }
 }
 
@@ -282,8 +321,8 @@ let INSTANCE = new Alloybot();
 
 const logger = new Logger('global');
 
-INSTANCE.on('module.registered', module => {
-  logger.info('Module Registered: ' + module.name);
+INSTANCE.on('plugin.registered', plugin => {
+  logger.info('Plugin Registered: ' + plugin.name);
 });
 
 INSTANCE.on('connection.registered', connection => {
